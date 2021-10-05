fb88bfed412c5e9e348db16445093f0a02f6d377
Admiral-White <mikeinyang975@gmail.com>
2021-10-06 00:58:52 +0100

    files and services folder created in Travel.Shared with the dependency injection completely done.

---
+++ src/infrastructure/Travel.Shared/DependencyInjection.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Travel.Application.Commons.Interfaces;
+using Travel.Domain.Settings;
+using Travel.Shared.Files;
+using Travel.Shared.Services;
+
+namespace Travel.Shared
+{
+    public class DependencyInjection
+    {
+        // i removed the this reference from the parameter bracket
+        public static IServiceCollection AddInfrastructureShared ( IServiceCollection services, IConfiguration config)
+        {
+            services.Configure<MailSettings>(config.GetSection("MailSettings"));
+            services.AddTransient<IDateTime, DateTimeService>();
+            services.AddTransient<IEmailService, EmailServices>();
+            services.AddTransient<ICsvFileBuilder, CsvFileBuilder>();
+
+            return services;
+        }
+    }
+}
